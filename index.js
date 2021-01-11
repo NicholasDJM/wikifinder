c1eec61a0bb41e9ab87383017fafa5454ae1ec08
Nicholas Miller <11604027+NicholasDJM@users.noreply.github.com>
2021-01-11 04:49:27 -0500

    Initial setup of i18n module. Switched line endings to LF instead CRLF.

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
 const chalk = require("chalk");
 const express = require("express");
 const path = require("path");
-const requestIp = require("request-ip");
+const {
+	I18n
+} = require("i18n");
 
-const port = process.env.npm_package_config_port; // eslint-disable-line no-undef
-const default_language = process.env.npm_package_config_default_language; // eslint-disable-line no-undef
-
-const app = express();
-app.use(requestIp.mw());
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views")); // eslint-disable-line no-undef
-
-app.use((req, res, next) => {
-	console.log(new Date().toLocaleString(), ">", req.method, chalk.yellow(req.url), chalk.bold.blue(req.clientIp));
-	next();
+const port = process.env.npm_package_config_port;
+const default_language = process.env.npm_package_config_default_language;
+const i18n = new I18n({
+	locales: ["en", "fr"],
+	directory: path.join(__dirname, "locales"),
+	objectNotation: true,
+	cookie: "desired-language"
 });
 
+const app = express();
+//app.use(requestIp.mw());
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use((req, res, next) => {
+	console.log(chalk.bold.green(new Date().toLocaleString()), req.method, chalk.yellow(req.url), chalk.bold.blue(req.ip));
+	next();
+});
+app.use(i18n.init);
+
+app.use(express.static(path.join(__dirname, "/public")));
 app.get("/", function(req, res) {
-	res.render("test", {
-		"title": "Test Page"
+	res.render("index");
+	console.log(res.__("title"));
+});
+
+app.use(function(req, res) {
+	let lang = default_language;
+	res.status(404).render("error", {
+		title: "Error 404",
+		error: 404,
+		terms: {
+			e404: "File not found"
+		},
+		lang: lang
+	});
+});
+app.use(function(req, res) {
+	let lang = default_language;
+	res.status(500).render("error", {
+		title: "Error 500",
+		error: 500,
+		terms: {
+			e500: "Internal Server Error"
+		},
+		lang: lang
 	});
 });
 
